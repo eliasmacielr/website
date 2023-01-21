21a19f4ef9c8b31ab5ba77c7f6353c67b9dd5bce
Elias Maciel <eliasmacielr@gmail.com>
2023-01-21 15:40:10 -0300

    Add files and dependencies for Vercel deployment

--- pages/contact-info.jsx
+++ pages/contact-info.jsx
@@ -13,19 +13,19 @@ export default function ContactInfo() {
         Paraguay
       </address>
       <br />
-      <a href="https://www.linkedin.com/in/eliasmacielr/" target="_blank">
+      <a href="https://www.linkedin.com/in/eliasmacielr/" target="_blank" rel="noreferrer">
         <img src="img/LI-In-Bug.png" alt="LinkedIn Logo" style={{ height: '32px' }}/>
       </a>
-      <a href="https://github.com/eliasmacielr" target="_blank">
+      <a href="https://github.com/eliasmacielr" target="_blank" rel="noreferrer">
         <img src="img/GitHub-Mark-32px.png" alt="GitHub Logo" style={{ height: '32px', width: '32px' }}/>
       </a>
-      <a href="https://twitter.com/eliasmacielr" target="_blank">
+      <a href="https://twitter.com/eliasmacielr" target="_blank" rel="noreferrer">
         <img src="img/Twitter_Social_Icon_Circle_Color.png" alt="Twitter Logo" style={{ height: '32px', width: '32px' }}/>
       </a>
-      <a href="https://www.facebook.com/profile.php?id=100080232595377" target="_blank">
+      <a href="https://www.facebook.com/profile.php?id=100080232595377" target="_blank" rel="noreferrer">
         <img src="img/f_logo_RGB-Blue_58.png" alt="Facebook Logo" style={{ height: '32px', width: '32px' }}/>
       </a>
-      <a href="https://www.instagram.com/eliasmacielcc/" target="_blank">
+      <a href="https://www.instagram.com/eliasmacielcc/" target="_blank" rel="noreferrer">
         <img src="img/Instagram_Glyph_Gradient.png" alt="Instagram Logo" style={{ height: '32px', width: '32px' }}/>
       </a>
       <br/><br/>
--- pages/courses-teaching.jsx
+++ pages/courses-teaching.jsx
@@ -18,16 +18,16 @@ export default function CoursesTeaching() {
         <dt>Programación Funcional en Haskell</dt>
         <dd>
           <a href="docs/programacion_funcional_en_haskell_programa.pdf" target="_blank">[course program]</a><br />
-          <a href="https://drive.google.com/open?id=1hKwQ8JaRDSUPoV-IUloNr6DpI3fbt8WT" target="_blank">[materials]</a>
+          <a href="https://drive.google.com/open?id=1hKwQ8JaRDSUPoV-IUloNr6DpI3fbt8WT" target="_blank" rel="noreferrer">[materials]</a>
         </dd>
       </dl>
 
-      <h2>Schools and seminars I've attended</h2>
+      <h2>Schools and seminars I&apos;ve attended</h2>
       <a href="https://www.ci2ma.udec.cl/epanum2019/">
         EPANUM 2019
       </a>
       <br />
-      <a href="http://www.cc.pol.una.py/index.php/2016/06/01/current-trends-in-data-science-seminar/" target="_blank">
+      <a href="http://www.cc.pol.una.py/index.php/2016/06/01/current-trends-in-data-science-seminar/" target="_blank" rel="noreferrer">
         Current Trends in Data Science
       </a>
       <br />
