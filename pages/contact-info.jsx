c99f7d0221ce49452bd7e384b6abeefa2178330c
Elias Maciel <eliasmacielr@gmail.com>
2023-01-22 00:39:36 -0300

    Use next/image for social media logos

--- pages/contact-info.jsx
+++ pages/contact-info.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import MainLayout from "../components/MainLayout";
 
 export default function ContactInfo() {
@@ -14,19 +15,19 @@ export default function ContactInfo() {
       </address>
       <br />
       <a href="https://www.linkedin.com/in/eliasmacielr/" target="_blank" rel="noreferrer">
-        <img src="img/LI-In-Bug.png" alt="LinkedIn Logo" style={{ height: '32px' }}/>
+        <Image src="/img/LI-In-Bug.png" alt="LinkedIn Logo" width={32} height={32} />
       </a>
       <a href="https://github.com/eliasmacielr" target="_blank" rel="noreferrer">
-        <img src="img/GitHub-Mark-32px.png" alt="GitHub Logo" style={{ height: '32px', width: '32px' }}/>
+        <Image src="/img/GitHub-Mark-32px.png" alt="GitHub Logo" width={32} height={32} />
       </a>
       <a href="https://twitter.com/eliasmacielr" target="_blank" rel="noreferrer">
-        <img src="img/Twitter_Social_Icon_Circle_Color.png" alt="Twitter Logo" style={{ height: '32px', width: '32px' }}/>
+        <Image src="/img/Twitter_Social_Icon_Circle_Color.png" alt="Twitter Logo" width={32} height={32} />
       </a>
       <a href="https://www.facebook.com/profile.php?id=100080232595377" target="_blank" rel="noreferrer">
-        <img src="img/f_logo_RGB-Blue_58.png" alt="Facebook Logo" style={{ height: '32px', width: '32px' }}/>
+        <Image src="/img/f_logo_RGB-Blue_58.png" alt="Facebook Logo" width={32} height={32} />
       </a>
       <a href="https://www.instagram.com/eliasmacielcc/" target="_blank" rel="noreferrer">
-        <img src="img/Instagram_Glyph_Gradient.png" alt="Instagram Logo" style={{ height: '32px', width: '32px' }}/>
+        <Image src="/img/Instagram_Glyph_Gradient.png" alt="Instagram Logo"  width={32} height={32} />
       </a>
       <br/><br/>
       <a href="https://data.typeracer.com/pit/profile?user=eliasmacielr&ref=badge"
