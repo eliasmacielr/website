730d3a9aad4ba095cb3e1819f8f0b73482a93ede
Elias Maciel <eliasmacielr@gmail.com>
2023-10-01 10:38:09 -0300

    Define global styles and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from 'tailwindcss/colors'
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'serif': ['CMU Serif', 'ui-serif', 'serif'],
+      'sans': ['ui-sans-serif', 'system-ui', 'sans-serif'],
+      'mono': ['monospace', 'SFMono-Regular', 'ui-monospace'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      slate: colors.slate,
+      stone: colors.stone,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      blue: colors.blue,
+    },
   },
   plugins: [],
 }
