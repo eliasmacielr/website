9a7e49ca8a8b91771504aeb0f47788eba50dcd88
Elias Maciel <eliasmacielr@gmail.com>
2023-10-19 16:36:25 -0300

    Remove nav order

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,19 +26,19 @@ export default function Header() {
         <h1 className="text-xl lg:text-5xl text-center pb-2 pt-4 lg:pt-16 lg:pb-6">Elias Maciel&apos;s Home Page</h1>
       </div>
       {/* Desktop nav */}
-      <div className="hidden lg:text-xl lg:flex flex-col lg:flex-row lg:justify-center lg:py-4 lg:gap-8">
-        <Link className="py-1 px-8 order-2" href="/projects">Projects</Link>
-        <Link className="py-1 px-8 order-4 mb-4 lg:mb-0" href="/contact-info">Contact &amp; Info</Link>
-        <Link className="py-1 px-8 order-3" href="/courses-teaching">Courses &amp; Teaching</Link>
-        <Link className="py-1 px-8 order-1" href="/">Home</Link>
+      <div className="hidden lg:text-xl lg:flex flex-col lg:flex-row lg:justify-center lg:py-4 lg:gap-8 mb-4 lg:mb-0">
+        <Link className="py-1 px-8" href="/">Home</Link>
+        <Link className="py-1 px-8" href="/projects">Projects</Link>
+        <Link className="py-1 px-8" href="/courses-teaching">Courses &amp; Teaching</Link>
+        <Link className="py-1 px-8" href="/contact-info">Contact &amp; Info</Link>
       </div>
       {/* Mobile nav */}
       <div className="topnav w-full lg:w-auto lg:hidden">
-        <div id="myLinks" className="hidden flex-col">
-          <Link className="py-1 px-8 order-2" href="/projects">Projects</Link>
-          <Link className="py-1 px-8 order-4 mb-4 lg:mb-0" href="/contact-info">Contact &amp; Info</Link>
-          <Link className="py-1 px-8 order-3" href="/courses-teaching">Courses &amp; Teaching</Link>
-          <Link className="py-1 px-8 order-1" href="/">Home</Link>
+        <div id="myLinks" className="hidden flex-col mb-4 lg:mb-0">
+          <Link className="py-1 px-8" href="/">Home</Link>
+          <Link className="py-1 px-8" href="/projects">Projects</Link>
+          <Link className="py-1 px-8" href="/courses-teaching">Courses &amp; Teaching</Link>
+          <Link className="py-1 px-8" href="/contact-info">Contact &amp; Info</Link>
         </div>
         <a href="#" className="absolute right-0 top-0 p-3.5" onClick={myFunction as MouseEventHandler<HTMLAnchorElement>}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
