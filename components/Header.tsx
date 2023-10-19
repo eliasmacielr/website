f481aec05969b2429dddb5a67d9275fa261f0611
Elias Maciel <eliasmacielr@gmail.com>
2023-10-19 16:14:50 -0300

    Center the header headline

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
         </h1>
       </div>
       <div className="max-w-screen-xl w-full">
-        <h1 className="text-xl lg:text-5xl text-left lg:text-center pl-10 pb-2 pt-4 lg:pt-16 lg:pb-6">Elias Maciel&apos;s Home Page</h1>
+        <h1 className="text-xl lg:text-5xl text-center pb-2 pt-4 lg:pt-16 lg:pb-6">Elias Maciel&apos;s Home Page</h1>
       </div>
       {/* Desktop nav */}
       <div className="hidden lg:text-xl lg:flex flex-col lg:flex-row lg:justify-center lg:py-4 lg:gap-8">
@@ -41,7 +41,7 @@ export default function Header() {
           <Link className="py-1 px-8 order-1" href="/">Home</Link>
         </div>
         <a href="#" className="absolute right-0 top-0 p-3.5" onClick={myFunction as MouseEventHandler<HTMLAnchorElement>}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
           </svg>
         </a>
