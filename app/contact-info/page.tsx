0d70c5b7ba5c9162154e48fd1c5350326fe281b4
Elias Maciel <eliasmacielr@gmail.com>
2023-10-21 10:59:23 -0300

    Run prettier

--- app/contact-info/page.tsx
+++ app/contact-info/page.tsx
@@ -4,42 +4,100 @@ import Image from "next/image";
 export default function ContactInfo() {
   return (
     <div className="px-8 lg:px-0">
-      <h2 id="contact-and-info" className="mb-4">Contact Info</h2>
+      <h2 id="contact-and-info" className="mb-4">
+        Contact Info
+      </h2>
 
       <address className=" mb-8">
-        Elias Maciel R.<br />
-        Google e-mail: <code>eliasmacielr at gmail dot com</code><br />
-        UNA institutional e-mail: <code>emaciel at pol dot una dot py</code><br />
-        Fernando de la Mora, Central<br />
+        Elias Maciel R.
+        <br />
+        Google e-mail: <code>eliasmacielr at gmail dot com</code>
+        <br />
+        UNA institutional e-mail: <code>emaciel at pol dot una dot py</code>
+        <br />
+        Fernando de la Mora, Central
+        <br />
         Paraguay
       </address>
 
-      <div className="w-fit flex flex-col gap-4 mb-8">
-        <Link href="https://www.linkedin.com/in/eliasmacielr/" target="_blank" rel="noreferrer" className="flex flex-row flex-grow-0 gap-4">
-          <Image src="/img/LI-In-Bug.png" alt="LinkedIn Logo" width={38} height={32} />
+      <div className="mb-8 flex w-fit flex-col gap-4">
+        <Link
+          href="https://www.linkedin.com/in/eliasmacielr/"
+          target="_blank"
+          rel="noreferrer"
+          className="flex flex-grow-0 flex-row gap-4"
+        >
+          <Image
+            src="/img/LI-In-Bug.png"
+            alt="LinkedIn Logo"
+            width={38}
+            height={32}
+          />
           eliasmacielr
         </Link>
-        <Link href="https://github.com/eliasmacielr" target="_blank" rel="noreferrer"  className="flex flex-row gap-4">
-          <Image src="/img/GitHub-Mark-32px.png" alt="GitHub Logo" width={32} height={32} />
+        <Link
+          href="https://github.com/eliasmacielr"
+          target="_blank"
+          rel="noreferrer"
+          className="flex flex-row gap-4"
+        >
+          <Image
+            src="/img/GitHub-Mark-32px.png"
+            alt="GitHub Logo"
+            width={32}
+            height={32}
+          />
           eliasmacielr
         </Link>
-        <Link href="https://x.com/eliasmacielr" target="_blank" rel="noreferrer"  className="flex flex-row gap-4">
+        <Link
+          href="https://x.com/eliasmacielr"
+          target="_blank"
+          rel="noreferrer"
+          className="flex flex-row gap-4"
+        >
           <Image src="/img/X_logo.png" alt="X Logo" width={32} height={32} />
           @eliasmacielr
         </Link>
-        <Link href="https://www.instagram.com/eliasmacielcc/" target="_blank" rel="noreferrer"  className="flex flex-row gap-4">
-          <Image src="/img/Instagram_Glyph_Gradient.png" alt="Instagram Logo"  width={32} height={32} />
+        <Link
+          href="https://www.instagram.com/eliasmacielcc/"
+          target="_blank"
+          rel="noreferrer"
+          className="flex flex-row gap-4"
+        >
+          <Image
+            src="/img/Instagram_Glyph_Gradient.png"
+            alt="Instagram Logo"
+            width={32}
+            height={32}
+          />
           @eliasmacielcc
         </Link>
-        <Link href="https://www.facebook.com/profile.php?id=100080232595377" target="_blank" rel="noreferrer"  className="flex flex-row gap-4">
-          <Image src="/img/f_logo_RGB-Blue_58.png" alt="Facebook Logo" width={32} height={32} />
+        <Link
+          href="https://www.facebook.com/profile.php?id=100080232595377"
+          target="_blank"
+          rel="noreferrer"
+          className="flex flex-row gap-4"
+        >
+          <Image
+            src="/img/f_logo_RGB-Blue_58.png"
+            alt="Facebook Logo"
+            width={32}
+            height={32}
+          />
           100080232595377
         </Link>
       </div>
 
-      <a href="https://data.typeracer.com/pit/profile?user=eliasmacielr&ref=badge"
-        target="_top"><img src="https://data.typeracer.com/misc/badge?user=eliasmacielr"
-          style={{border: 0}} alt="TypeRacer.com scorecard for user eliasmacielr"/></a>
+      <a
+        href="https://data.typeracer.com/pit/profile?user=eliasmacielr&ref=badge"
+        target="_top"
+      >
+        <img
+          src="https://data.typeracer.com/misc/badge?user=eliasmacielr"
+          style={{ border: 0 }}
+          alt="TypeRacer.com scorecard for user eliasmacielr"
+        />
+      </a>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,25 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="px-8 lg:px-0">
-      <section className="lg:grid lg:grid-cols-3 mb-16">
-        <h2 id="interests" className="italic mb-4 lg:mb-0">Interests</h2>
+      <section className="mb-16 lg:grid lg:grid-cols-3">
+        <h2 id="interests" className="mb-4 italic lg:mb-0">
+          Interests
+        </h2>
         <div className="col-span-2">
-          <p>My main areas of interest are web development and computer science.</p>
-          <p>I study music with the bass guitar and practice Brazilian Jiu-Jitsu.</p>
+          <p>
+            My main areas of interest are web development and computer science.
+          </p>
+          <p>
+            I study music with the bass guitar and practice Brazilian Jiu-Jitsu.
+          </p>
         </div>
       </section>
-      <section className="lg:grid lg:grid-cols-3 mb-16">
-        <h2 id="work" className="italic mb-4 lg:mb-0" >Work</h2>
+      <section className="mb-16 lg:grid lg:grid-cols-3">
+        <h2 id="work" className="mb-4 italic lg:mb-0">
+          Work
+        </h2>
         <div className="col-span-2">
           <dl className="mb-8">
             <dt>
@@ -47,7 +55,9 @@ export default function Home() {
           </dl>
           <dl className="mb-8">
             <dt>
-              <p className="font-bold">Cluster de Investigación Aplicada (CIA) &mdash; FP-UNA</p>
+              <p className="font-bold">
+                Cluster de Investigación Aplicada (CIA) &mdash; FP-UNA
+              </p>
             </dt>
             <dd>
               <p>Junior Software Analyst, September-December 2015.</p>
@@ -55,17 +65,24 @@ export default function Home() {
           </dl>
         </div>
       </section>
-      <section className="lg:grid lg:grid-cols-3 mb-16">
-        <h2 id="education" className="mb-4 lg:mb-0">Education</h2>
+      <section className="mb-16 lg:grid lg:grid-cols-3">
+        <h2 id="education" className="mb-4 lg:mb-0">
+          Education
+        </h2>
         <div className="col-span-2">
           <dl className="mb-8">
             <dt>
               <p>National University of Asunción</p>
             </dt>
             <dd>
-              <p>Master of Science in Computer Science, 2021<br />
-              Advisors: Christian Schaerer and Inocencio Ortiz<br />
-              Thesis: Proposal of Geometric Integrator for Nonconservative, Nonholonomic Mechanical Systems</p>
+              <p>
+                Master of Science in Computer Science, 2021
+                <br />
+                Advisors: Christian Schaerer and Inocencio Ortiz
+                <br />
+                Thesis: Proposal of Geometric Integrator for Nonconservative,
+                Nonholonomic Mechanical Systems
+              </p>
             </dd>
           </dl>
           <dl>
@@ -73,91 +90,172 @@ export default function Home() {
               <p>National University of Asunción</p>
             </dt>
             <dd>
-              <p>Bachelor of Science in Information Technology, 2017<br />
-              Advisor: Prof. Christian Schaerer<br />
-              Thesis: Evaluation of Block Conjugate Gradient for a Single Right-Hand Side <Link href="http://www.cc.pol.una.py/~cschaer/Tesis-Elias%20Maciel.pdf">[PDF]</Link></p>
+              <p>
+                Bachelor of Science in Information Technology, 2017
+                <br />
+                Advisor: Prof. Christian Schaerer
+                <br />
+                Thesis: Evaluation of Block Conjugate Gradient for a Single
+                Right-Hand Side{" "}
+                <Link href="http://www.cc.pol.una.py/~cschaer/Tesis-Elias%20Maciel.pdf">
+                  [PDF]
+                </Link>
+              </p>
             </dd>
           </dl>
         </div>
       </section>
-      <section className="lg:grid lg:grid-cols-3 mb-16">
+      <section className="mb-16 lg:grid lg:grid-cols-3">
         <h2 id="publications">Publications</h2>
         <div className="col-span-2">
           <ul>
             <li className="my-4">
-              <h3>A Herglotz-based integrator for nonholonomic mechanical systems</h3>
-              <Link className="truncate text-ellipsis w-50 block" href="https://www.aimspress.com/article/doi/10.3934/jgm.2023012" target="_blank">https://www.aimspress.com/article/doi/10.3934/jgm.2023012</Link>
+              <h3>
+                A Herglotz-based integrator for nonholonomic mechanical systems
+              </h3>
+              <Link
+                className="w-50 block truncate text-ellipsis"
+                href="https://www.aimspress.com/article/doi/10.3934/jgm.2023012"
+                target="_blank"
+              >
+                https://www.aimspress.com/article/doi/10.3934/jgm.2023012
+              </Link>
               <p>February 2023</p>
             </li>
           </ul>
           <ul>
             <li>
-              <h3>A comparison of geometric integrators for nonconservative and nonholonomic systems</h3>
-              <Link className="truncate text-ellipsis w-50 block" href="https://cilamce.com.br/anais/arearestrita/apresentacoes/182/8449.pdf" target="_blank">https://cilamce.com.br/anais/arearestrita/apresentacoes/182/8449.pdf</Link>
+              <h3>
+                A comparison of geometric integrators for nonconservative and
+                nonholonomic systems
+              </h3>
+              <Link
+                className="w-50 block truncate text-ellipsis"
+                href="https://cilamce.com.br/anais/arearestrita/apresentacoes/182/8449.pdf"
+                target="_blank"
+              >
+                https://cilamce.com.br/anais/arearestrita/apresentacoes/182/8449.pdf
+              </Link>
               <p>November 2020</p>
             </li>
           </ul>
         </div>
       </section>
-      <section className="lg:grid lg:grid-cols-3 mb-16">
-        <h2 id="conferences-and-meetings" className="mb-4 lg:mb-0">Conferences and meetings</h2>
+      <section className="mb-16 lg:grid lg:grid-cols-3">
+        <h2 id="conferences-and-meetings" className="mb-4 lg:mb-0">
+          Conferences and meetings
+        </h2>
         <div className="col-span-2">
           <ul>
             <li>
-              CILAMCE 2020, XLI Ibero-Latin American Congress on Computational Methods in Engineering,
-              16 - 19 November, 2020 - On-line.
+              CILAMCE 2020, XLI Ibero-Latin American Congress on Computational
+              Methods in Engineering, 16 - 19 November, 2020 - On-line.
             </li>
             <li>
-              <Link href="http://sociedadcientifica.org.py/encuentrodeinvestigadores-convocatoria-2020/" target="_blank">V Encuentro de Investigadores</Link>,
-              9 - 12 de noviembre de 2020 - Online.
+              <Link
+                href="http://sociedadcientifica.org.py/encuentrodeinvestigadores-convocatoria-2020/"
+                target="_blank"
+              >
+                V Encuentro de Investigadores
+              </Link>
+              , 9 - 12 de noviembre de 2020 - Online.
             </li>
             <li>
-              <Link href="http://sociedadcientifica.org.py/un-encuentro-para-celebrar-y-discutir-la-ciencia-que-se-construye-en-paraguay/" target="_blank">IV Encuentro de Investigadores</Link>,
-              6 - 8 de noviembre de 2019 - Sociedad Científica del Paraguay, Asunción, Paraguay.
+              <Link
+                href="http://sociedadcientifica.org.py/un-encuentro-para-celebrar-y-discutir-la-ciencia-que-se-construye-en-paraguay/"
+                target="_blank"
+              >
+                IV Encuentro de Investigadores
+              </Link>
+              , 6 - 8 de noviembre de 2019 - Sociedad Científica del Paraguay,
+              Asunción, Paraguay.
             </li>
             <li>
-              <Link href="http://sociedadcientifica.org.py/el-22-de-agosto-inicia-el-ii-encuentro-de-investigadores/" target="_blank">II Encuentro de investigadores</Link>,
-              22 - 25 de agosto de 2017 - Sociedad Científica del Paraguay, Asunción, Paraguay.
+              <Link
+                href="http://sociedadcientifica.org.py/el-22-de-agosto-inicia-el-ii-encuentro-de-investigadores/"
+                target="_blank"
+              >
+                II Encuentro de investigadores
+              </Link>
+              , 22 - 25 de agosto de 2017 - Sociedad Científica del Paraguay,
+              Asunción, Paraguay.
             </li>
             <li>
-              <Link href="http://www.cc.pol.una.py/events/metting-in-applied-scientific-computing/" target="_blank">Meeting in Applied Scientific Computing</Link>,
-              Thursday 20/July/2017 9:00 hs NIDTEC - Facultad Politécnica - San Lorenzo, Paraguay.
+              <Link
+                href="http://www.cc.pol.una.py/events/metting-in-applied-scientific-computing/"
+                target="_blank"
+              >
+                Meeting in Applied Scientific Computing
+              </Link>
+              , Thursday 20/July/2017 9:00 hs NIDTEC - Facultad Politécnica -
+              San Lorenzo, Paraguay.
             </li>
             <li>
-              <Link href="http://www.epacis.net/ccis2016/en/index.php" target="_blank">
-              CCIS 2016 - 4th Conference of Computational Interdisciplinary Sciences</Link>,
-              November 7th - 10th, 2016 - São José dos Campos, SP, Brazil.
+              <Link
+                href="http://www.epacis.net/ccis2016/en/index.php"
+                target="_blank"
+              >
+                CCIS 2016 - 4th Conference of Computational Interdisciplinary
+                Sciences
+              </Link>
+              , November 7th - 10th, 2016 - São José dos Campos, SP, Brazil.
             </li>
           </ul>
         </div>
       </section>
-      <section className="lg:grid lg:grid-cols-3 mb-16">
-        <h2 id="posters-and-presentations" className="mb-4 lg:mb-0">Posters and presentations</h2>
+      <section className="mb-16 lg:grid lg:grid-cols-3">
+        <h2 id="posters-and-presentations" className="mb-4 lg:mb-0">
+          Posters and presentations
+        </h2>
         <div className="col-span-2">
           <ul>
             <li>
-              <Link href="docs/Integrador_geom_trico_de_contacto_con_restricciones_no_holon_micas___Poster_V_Encuentro_de_Investigadores_2020.pdf" target="_blank">Integrador geométrico de contacto con restricciones no holonómicas</Link>
+              <Link
+                href="docs/Integrador_geom_trico_de_contacto_con_restricciones_no_holon_micas___Poster_V_Encuentro_de_Investigadores_2020.pdf"
+                target="_blank"
+              >
+                Integrador geométrico de contacto con restricciones no
+                holonómicas
+              </Link>
             </li>
             <li>
-              <Link href="docs/Presentaci_n_de_Integrador_de_Contacto_para_el_Disco_Controlado_que_rueda_sin_deslizamiento.pdf" target="_blank">Integrador de Contacto para el Disco Controlado que rueda sin deslizamiento</Link>
+              <Link
+                href="docs/Presentaci_n_de_Integrador_de_Contacto_para_el_Disco_Controlado_que_rueda_sin_deslizamiento.pdf"
+                target="_blank"
+              >
+                Integrador de Contacto para el Disco Controlado que rueda sin
+                deslizamiento
+              </Link>
             </li>
             <li>
-              <Link href="docs/SpMV_and_SpMM_Performance_Measurement___CCIS_2016.pdf" target="_blank">SpMV and SpMM Performance Measurement</Link>
+              <Link
+                href="docs/SpMV_and_SpMM_Performance_Measurement___CCIS_2016.pdf"
+                target="_blank"
+              >
+                SpMV and SpMM Performance Measurement
+              </Link>
             </li>
             <li>
-              <Link href="docs/WPFG___Presentaci_n.pdf" target="_blank">Evaluación de las rutinas de multiplicación sobre matrices dispersas</Link>
+              <Link href="docs/WPFG___Presentaci_n.pdf" target="_blank">
+                Evaluación de las rutinas de multiplicación sobre matrices
+                dispersas
+              </Link>
             </li>
           </ul>
         </div>
       </section>
-      <section className="lg:grid lg:grid-cols-3 mb-16">
-        <h2 id="awards" className="mt-4 mb-6 lg:mb-0 lg:mt-0">Awards</h2>
+      <section className="mb-16 lg:grid lg:grid-cols-3">
+        <h2 id="awards" className="mb-6 mt-4 lg:mb-0 lg:mt-0">
+          Awards
+        </h2>
         <div className="col-span-2">
           <ul>
-            <li><b>Merit Honor</b>, National University of Asunción - 2018.</li>
+            <li>
+              <b>Merit Honor</b>, National University of Asunción - 2018.
+            </li>
           </ul>
         </div>
       </section>
     </div>
-  )
+  );
 }
