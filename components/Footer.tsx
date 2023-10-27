c91f7fc47d1ec44230e6dae5b7a9749ced6ca3bc
Elias Maciel <eliasmacielr@gmail.com>
2023-10-27 14:48:10 -0300

    Fix position of Y and X axis arrows

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,8 +7,8 @@ export default function Footer() {
       <p className='p-12 text-3xl'>
         Built with Brain &amp; Brawn by Elias Maciel - {year}.
       </p>
-      <div className="absolute bottom-0 left-0">
-        <svg className="mb-3" id='move-y' xmlns="http://www.w3.org/2000/svg" width="20px" height="19px" viewBox="0 -442 490 647" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" style={{}}>
+      <div className="fixed bottom-0 left-0">
+        <svg className="mb-4" id='move-y' xmlns="http://www.w3.org/2000/svg" width="20px" height="19px" viewBox="0 -442 490 647" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" style={{}}>
           <defs>
             <path id="MJX-13-TEX-I-1D466" d="M21 287Q21 301 36 335T84 406T158 442Q199 442 224 419T250 355Q248 336 247 334Q247 331 231 288T198 191T182 105Q182 62 196 45T238 27Q261 27 281 38T312 61T339 94Q339 95 344 114T358 173T377 247Q415 397 419 404Q432 431 462 431Q475 431 483 424T494 412T496 403Q496 390 447 193T391 -23Q363 -106 294 -155T156 -205Q111 -205 77 -183T43 -117Q43 -95 50 -80T69 -58T89 -48T106 -45Q150 -45 150 -87Q150 -107 138 -122T115 -142T102 -147L99 -148Q101 -153 118 -160T152 -167H160Q177 -167 186 -165Q219 -156 247 -127T290 -65T313 -9T321 21L315 17Q309 13 296 6T270 -6Q250 -11 231 -11Q185 -11 150 11T104 82Q103 89 103 113Q103 170 138 262T173 379Q173 380 173 381Q173 390 173 393T169 400T158 404H154Q131 404 112 385T82 344T65 302T57 280Q55 278 41 278H27Q21 284 21 287Z">
             </path>
@@ -26,7 +26,8 @@ export default function Footer() {
               </path>
           </defs><g stroke="red" fill="red" stroke-width="0" transform="scale(1,-1)"><g data-mml-node="math"><g data-mml-node="mi"><use data-c="25B4" href="#MJX-12-TEX-I-25B4"></use></g></g></g>
         </svg>
-        
+      </div>
+      <div className="fixed bottom-0 left-0">
         <svg className="" height="7" width="30" id='expand-right'>
           <line x1="0" y1="0" x2="100vw" y2="0" style={{stroke:'rgb(255,0,0)', strokeWidth:10}} />
           Sorry, your browser does not support inline SVG.
