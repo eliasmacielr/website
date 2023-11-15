fbde7b71fdf0d72790a1d8a90a2a72f6acd8c6a7
Elias Maciel <eliasmacielr@gmail.com>
2023-11-15 17:52:21 -0300

    Separate X and Y axis into their own components

---
+++ components/YAxis.tsx
@@ -0,0 +1,82 @@
+export default function YAxis() {
+  return (
+    <div className='fixed bottom-0 left-0'>
+      <svg
+        className='relative bottom-10 left-10 text-red-600 '
+        id='move-y'
+        xmlns='http://www.w3.org/2000/svg'
+        width='21.507px'
+        height='29.28px'
+        viewBox='0 -442 490 647'
+        xmlnsXlink='http://www.w3.org/1999/xlink'
+        aria-hidden='true'
+        style={{}}
+      >
+        <defs>
+          <path
+            id='MJX-13-TEX-I-1D466'
+            d='M21 287Q21 301 36 335T84 406T158 442Q199 442 224 419T250 355Q248 336 247 334Q247 331 231 288T198 191T182 105Q182 62 196 45T238 27Q261 27 281 38T312 61T339 94Q339 95 344 114T358 173T377 247Q415 397 419 404Q432 431 462 431Q475 431 483 424T494 412T496 403Q496 390 447 193T391 -23Q363 -106 294 -155T156 -205Q111 -205 77 -183T43 -117Q43 -95 50 -80T69 -58T89 -48T106 -45Q150 -45 150 -87Q150 -107 138 -122T115 -142T102 -147L99 -148Q101 -153 118 -160T152 -167H160Q177 -167 186 -165Q219 -156 247 -127T290 -65T313 -9T321 21L315 17Q309 13 296 6T270 -6Q250 -11 231 -11Q185 -11 150 11T104 82Q103 89 103 113Q103 170 138 262T173 379Q173 380 173 381Q173 390 173 393T169 400T158 404H154Q131 404 112 385T82 344T65 302T57 280Q55 278 41 278H27Q21 284 21 287Z'
+          ></path>
+        </defs>
+        <g
+          stroke='currentcolor'
+          fill='currentcolor'
+          stroke-width='0'
+          transform='scale(1,-1)'
+        >
+          <g data-mml-node='math'>
+            <g data-mml-node='mi'>
+              <use data-c='1D466' href='#MJX-13-TEX-I-1D466'></use>
+            </g>
+          </g>
+        </g>
+      </svg>
+      <svg
+        className='absolute bottom-0 left-1.5 text-red-600'
+        height='30'
+        width='7'
+        id='expand-up'
+      >
+        <line
+          x1='0'
+          y1='0'
+          x2='0'
+          y2='100vh'
+          style={{ stroke: 'currentcolor', strokeWidth: 10 }}
+        />
+        Sorry, your browser does not support inline SVG.
+      </svg>
+
+      <svg
+        className='relative text-red-600'
+        id='arrow-up'
+        xmlns='http://www.w3.org/2000/svg'
+        width='22px'
+        height='16.1636px'
+        viewBox='0 -575 722 595'
+        xmlnsXlink='http://www.w3.org/1999/xlink'
+        aria-hidden='true'
+        style={{}}
+      >
+        <defs>
+          <path
+            id='MJX-12-TEX-I-25B4'
+            d='M99 -20Q84 -11 84 0Q84 5 148 145T278 424L342 563Q347 575 360 575Q368 575 375 570Q376 569 441 430T571 148T637 0Q637 -11 622 -20H99Z'
+          ></path>
+        </defs>
+        <g
+          stroke='currentcolor'
+          fill='currentcolor'
+          stroke-width='0'
+          transform='scale(1,-1)'
+        >
+          <g data-mml-node='math'>
+            <g data-mml-node='mi'>
+              <use data-c='25B4' href='#MJX-12-TEX-I-25B4'></use>
+            </g>
+          </g>
+        </g>
+      </svg>
+    </div>
+  )
+}
