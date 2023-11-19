8c7f77b7640fa0f11d69d576134e440157b8a6a6
Elias Maciel <eliasmacielr@gmail.com>
2023-11-18 23:16:03 -0300

    Use viewport export instead of metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,15 +29,16 @@ export const metadata: Metadata = {
   authors: [{ name: 'Elias Maciel', url: 'eliasmacielr.org' }],
   creator: 'Elias Maciel',
   publisher: 'Elias Maciel',
-  colorScheme: 'light',
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-  },
   icons: {
     icon: '/favicon.svg',
   },
+}
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  colorScheme: 'light',
 }
 
 export default function RootLayout({
