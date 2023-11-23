87bc6266bc87ad2547743804f1d1d33737e18b2c
Elias Maciel <eliasmacielr@gmail.com>
2023-11-23 18:34:22 -0300

    Implement inner width and height measurements

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,47 @@
+'use client'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const size = useWindowSize()
+
+  function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState({
+      width: 0,
+      height: 0,
+    })
+
+    useEffect(() => {
+      // only execute all the code below in client side
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        })
+      }
+
+      // Add event listener
+      window.addEventListener("resize", handleResize)
+
+      // Call handler right away so state gets updated with initial window size
+      handleResize()
+
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize)
+    }, []) // Empty array ensures that effect is only run on mount
+
+    return windowSize
+  }
+
   return (
     <div className='px-8 lg:px-0'>
+      <div>
+        {size.width}px / {size.height}px
+      </div>
       <section className='mb-16 lg:grid lg:grid-cols-3'>
         <h2 id='interests' className='mb-4 italic lg:mb-0'>
           Interests
